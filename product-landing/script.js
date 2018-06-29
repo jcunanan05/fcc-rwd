386751f5ddb06e3c0586b945befdce3390f255bf
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-06-28 21:35:45 -0400

    Pass fCC Test

--- product-landing/script.js
+++ product-landing/script.js
@@ -1,10 +1,21 @@
 var Product = function() {
   var burger = document.querySelector('.navbar-burger');
   var navbarMenu = document.querySelector('.navbar-menu');
+  var navbarItems = document.querySelectorAll('.navbar-item');
 
-  burger.addEventListener('click', function() {
-    navbarMenu.classList.toggle('navbar-menu--active');
+  burger.addEventListener('click', toggleNavbarMenu);
+
+  navbarItems.forEach(function(navbarItem) {
+    navbarItem.addEventListener('click', hideNavbarMenu);
   });
+
+  function hideNavbarMenu() {
+    navbarMenu.classList.remove('navbar-menu--active');
+  }
+
+  function toggleNavbarMenu() {
+    navbarMenu.classList.toggle('navbar-menu--active');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
